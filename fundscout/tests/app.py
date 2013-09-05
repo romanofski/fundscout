28e84a03c2eb79261dc60e5b38e7b1cfadf8deda
Róman Joost <roman@bromeco.de>
2013-09-06 08:15:17 +1000

    Bootstrapping application for testing browser.

--- fundscout/tests/app.py
+++ fundscout/tests/app.py
@@ -1,16 +1,38 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import url_for
+from flask import redirect
 
 
 app = Flask(__name__)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        error = 'Invalid username'
+        error = validate_login(request.form['user'],
+                               request.form['password'])
+        if not error:
+            return redirect(url_for('download'))
     return render_template('login.html', error=error)
+
+
+@app.route('/download', methods=['GET', 'POST'])
+def download():
+    return render_template('download.html')
+
+
+def validate_login(user, password):
+    result = []
+    if len(user) != 6:
+        result.append('Username has to be 6 characters')
+    if len(password) <= 6 or len(password) >= 12:
+        result.append(
+            'Password needs to be at least 6 not more than 12 characters long')
+    return ', '.join(result)
 
 
 def run_server():
--- fundscout/tests/test_import_integration.py
+++ fundscout/tests/test_import_integration.py
@@ -4,13 +4,15 @@ import fundscout.importer.config
 import ghost
 
 
+URL = u'http://localhost:5000/'
+
+
 class TestDownloadCSV(unittest.TestCase):
 
     layer = IntegrationLayer
 
-    def test_basic(self):
-        url = u'http://localhost:5000/login'
-        step = fundscout.importer.config.open([url])
+    def test_login_and_download(self):
+        step = fundscout.importer.config.open([URL])
         browser = ghost.Ghost()
         page, resources = step(browser)
         self.assertTrue('User' in browser.content)
