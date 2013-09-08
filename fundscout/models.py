e1aed452cb7edacd83494e8c3e98c3a2d1504bbd
Róman Joost <roman@bromeco.de>
2013-09-08 12:49:24 +1000

    Wrap each transaction in an ImportBatch
    With this, we want to allow, that users can roll back or re-import sets
    of transactions. This may be useful if some of the transactions data are
    not correctly created by importers.

--- fundscout/models.py
+++ fundscout/models.py
@@ -1,10 +1,12 @@
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import Numeric
+from sqlalchemy import Date
 from sqlalchemy import String
 from sqlalchemy import ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 import sqlalchemy.orm
+import datetime
 
 
 Base = declarative_base()
@@ -21,7 +23,19 @@ class Currency(Base):
     transaction_id = Column(Integer, ForeignKey('bankaccount.id'))
 
 
-class Transaction(Base):
+class ImportBatch(Base):
+    """A batch of transactions we're importing."""
+    __tablename__ = 'importbatch'
+
+    id = Column(Integer, primary_key=True)
+    import_date = Column(Date, default=datetime.date.today())
+    bank_account_id = Column(Integer, ForeignKey('bankaccount.id'))
+
+    transactions = sqlalchemy.orm.relationship(
+        'FundTransaction', backref='importbatch')
+
+
+class FundTransaction(Base):
     """A simple transaction of funds."""
     __tablename__ = 'transaction'
 
@@ -29,6 +43,7 @@ class Transaction(Base):
     description = Column(String)
     amount = Column(Numeric)
 
+    import_batch_id = Column(Integer, ForeignKey('importbatch.id'))
 
 
 class BankAccount(Base):
@@ -39,3 +54,15 @@ class BankAccount(Base):
     description = Column(String)
     currency = sqlalchemy.orm.relationship(
         'Currency', uselist=False, backref='bankaccount')
+
+    import_batches = sqlalchemy.orm.relationship(
+        'ImportBatch', backref='bankaccount')
+
+    def import_transactions(self, list_of_transactions):
+        """Wraps the list of transactions in an ImportBatch in order to
+           allow easy importing/rolling back of transactions.
+
+           Note: This should not be confused
+        """
+        batch = ImportBatch(transactions=list_of_transactions)
+        self.import_batches.append(batch)
--- fundscout/tests/test_account.py
+++ fundscout/tests/test_account.py
@@ -1,6 +1,7 @@
 from fundscout.models import BankAccount
 from fundscout.models import Currency
 from fundscout.models import Session
+from fundscout.models import FundTransaction
 import fundscout
 import fundscout.testing
 import unittest
@@ -25,3 +26,15 @@ class TestAccountFunctional(unittest.TestCase):
         account = session.query(BankAccount).first()
         self.assertEqual('Test Description', account.description)
         self.assertEqual('EUR', account.currency.isoname)
+
+    def test_import_rollback_batch(self):
+        session = Session()
+        account = BankAccount(description='Test Description',
+                              currency=session.query(Currency).first())
+        transactions = [
+            FundTransaction(description='first', amount=-2.30),
+            FundTransaction(description='second', amount=10.02),
+        ]
+        account.import_transactions(transactions)
+
+        self.assertEqual(1, len(account.import_batches))
